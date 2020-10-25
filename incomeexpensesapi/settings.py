076655d9d2d8a030e9f32041f251b4779571a3e0
grigorii-sum <36480336+grigorii-sum@users.noreply.github.com>
2020-10-25 16:52:57 +0300

    Update settings.py

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(weeks=1),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
 }
 
